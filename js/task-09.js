cf4160d3e48fd72963fd11a3097ced1db0d0e25f
IhorChepak <chepak_lex@ukr.net>
2023-03-27 22:22:41 +0300

    ДЗ 6

--- js/task-09.js
+++ js/task-09.js
@@ -9,6 +9,6 @@ button.addEventListener('click', () => {
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+        .toString(16)
+        .padStart(6, 0)}`;
 }
